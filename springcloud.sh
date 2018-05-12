5f0c8796d3911e6eb37339b7ffb99c4e4167872f
Carlos Castro <carlos_o_castro@yahoo.com.ar>
2018-05-12 12:28:43 -0300

    Update Network

--- springcloud.sh
+++ springcloud.sh
@@ -1,1 +1,1 @@
-docker run   -ti  --rm --name=springcloud -p 8888:8888 -p 8761:8761 -p 9095:9095 -p 9092:9092 -p 9411:9411 -p 9091:9091 springcloud cloud $@
+docker run --network host  -ti  --rm --name=springcloud -p 8888:8888 -p 8761:8761 -p 9095:9095 -p 9092:9092 -p 9411:9411 -p 9091:9091 springcloud cloud $@
